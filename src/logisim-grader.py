6bcc31937da85f26d6f789993c88beab08b1370a
lucaFiorini <luca.fio45@gmail.com>
2026-01-31 00:55:29 +0100

    reorganize LogisimProject

--- src/logisim-grader.py
+++ src/logisim-grader.py
@@ -1,27 +1,64 @@
 import subprocess
-from dataclasses import dataclass
+from dataclasses import field, dataclass
+from typing import Self
 import xml.etree.ElementTree as ElementTree
-
-class LANGS:
-    ENG="eng"
-    ITA="ita"
-
-lang = LANGS.ITA
 
 @dataclass
 class LogisimProject:
+
+    class NotAnInputException(Exception):
+        pass
+    
     @dataclass
     class Circuit:
-        @dataclass
+        @dataclass(kw_only=True,frozen=True)
         class Component:
-            def __init__(self,root: ElementTree.Element):
-                self.root = root
-        @dataclass
+            root : ElementTree.Element[str]
+            label : str = field(init=False)
+
+            def __post_init__(self) -> None:
+                for attr in self.root:
+                    if attr.get('label'):
+                        object.__setattr__(self,'label', attr.get('val'))
+                        return
+                        
+
+        @dataclass(kw_only=True,frozen=True)
         class Input(Component):
-            pass
-        @dataclass
+            __tristate : ElementTree.Element[str] = field(init=False)
+            
+            def __post_init__(self) -> None:
+                ok = False
+                for attr in self.root:
+                    if attr.get('tristate') is not None:
+                        ok = True
+                        object.__setattr__(self,'__tristate', attr)
+                        break
+                
+                assert ok, 'Failed to initialize Input'
+
+            def get_value(self) -> bool:
+                return self.__tristate.get('val') == 'true'
+            
+            def set_value(self, value : bool) -> None:
+                self.__tristate.set('val','true' if value else 'false')
+
+        @dataclass(kw_only=True,frozen=True)
         class Output(Component):
-            pass
+            __value : bool = field(init=False)
+
+            def __post_init__(self) -> None:
+                ok = False
+                for attr in self.root:
+                    if attr.get('output') is not None:
+                        ok = True
+                        object.__setattr__(self,'__value',attr.get('val'))
+                        break
+                
+                assert ok, 'Failed to initialize Output'
+            
+            def get_value(self) -> bool:
+                return self.__value
             
     def __init__(self,fname : str):
         self.tree = ElementTree.parse(fname)
@@ -29,21 +66,25 @@ class LogisimProject:
         self.outputs : list[LogisimProject.Circuit.Component]
         self.inputs : list[LogisimProject.Circuit.Component]
         
-        circuits = self.tree.findall("circuit")
-        assert len(circuits) > 0, "No circuits found"
+        circuits = self.tree.findall('circuit')
+        assert len(circuits) > 0, 'No circuits found'
         
         main_circuit = None
         for circuit in circuits:
-            circuit_name = circuit.get("name")
-            if circuit_name is not None and circuit_name == "main":
+            circuit_name = circuit.get('name')
+            if circuit_name is not None and circuit_name == 'main':
                 main_circuit = circuit
                 break
         
-        assert main_circuit is not None, "No main circuit found"
+        assert main_circuit is not None, 'No main circuit found'
         
-        components = main_circuit.findall("componenet")
+        components = main_circuit.findall('componenet')
         
         for component in components:
-            if component.get("name") is not None and  component.get("name") == "Pin":
-                for children in component:
-                    pass #TODO
+            if component.get('name') is not None and component.get('name') == 'Pin':
+                for attribute in component:
+                    component_type = attribute.get('name') 
+                    if component_type == 'tristate':
+                        self.inputs.append(LogisimProject.Circuit.Input(root=component))
+                    elif component_type == 'output': 
+                        self.inputs.append(LogisimProject.Circuit.Output(root=component))
