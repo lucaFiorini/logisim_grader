7114fa34791c3913d24466b2d55b6c49f95aa8a4
lucaFiorini <luca.fio45@gmail.com>
2026-03-31 21:58:00 +0200

    fix use absolute path to circuit

--- circuit_generator.py
+++ circuit_generator.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
+import os
 from pathlib import Path
 from .logisim_types import Coord,Direction,Wire,Output
 import xml.etree.ElementTree as ET
 from importlib.resources import files,as_file
 
 #The filename of the placeholder circuit to be replaced in the tester project
-CIRC_TO_REPLACE = Path(str(__path__),'placeholder.circ')
+CIRC_TO_REPLACE = Path(os.getcwd(),'placeholder.circ')
   
 def get_tester_circuit_resource():
   return as_file(files(__package__).joinpath("circuits/tester.circ"))
